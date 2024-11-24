03449d5fd29bddb437a2893a574700b134bd0fc3
stradivirus <stradivirus9@gmail.com>
2024-11-25 08:44:17 +0900

    [up] 버킷 업로드 test9

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://storage.googleapis.com/exam-serverless-storage/',
+  base: 'https://storage.googleapis.com/exam-serverless-storage/dist/',
   build: {
     sourcemap: false
   }
