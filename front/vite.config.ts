ada0134baa90339354cf67b287465b12ac575b72
stradivirus <stradivirus9@gmail.com>
2024-11-25 08:35:59 +0900

    [up] 버킷 업로드 test5

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/dist/',  // './' 대신 '/dist/'로 수정
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist'
+  }
 })
