3aa327671e56ff4e56177d417a338f64d26cb719
stradivirus <stradivirus9@gmail.com>
2024-11-25 08:56:57 +0900

    [up] 버킷 업로드 test12

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',  // base를 비워두고
+  base: 'https://storage.googleapis.com/exam-serverless-storage/',  // 버킷 루트 URL로 설정
   build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
+    sourcemap: false
   }
 })
