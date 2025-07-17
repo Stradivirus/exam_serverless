d460ed22d56034fab5877f762a7a3f15b03659af
root <root@DESKTOP-I98R5CS.localdomain>
2025-07-17 16:00:50 +0900

    [up2] ㅅㄷㄴt2

--- GCP/main.go
+++ GCP/main.go
@@ -9,6 +9,6 @@ import (
 
 func main() {
 	http.Handle("/", http.HandlerFunc(examhandler.ExamHandler))
-	log.Println("서버 시작: http://localhost:8080")
+	log.Println("서버 시작")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
