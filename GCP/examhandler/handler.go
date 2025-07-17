a7d97923282c871b8507dccb66e65e08e755e3ff
root <root@DESKTOP-I98R5CS.localdomain>
2025-07-17 15:46:48 +0900

    test 3

--- GCP/examhandler/handler.go
+++ GCP/examhandler/handler.go
@@ -97,9 +97,3 @@ func ExamHandler(w http.ResponseWriter, r *http.Request) {
 		sendError(w, "Invalid action", http.StatusBadRequest)
 	}
 }
-
-func setCORSHeaders(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
