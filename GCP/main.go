21964de1df9e0aff6a1e3fc5da60f229a93482e7
Super User <root@DESKTOP-4G7I13F.localdomain>
2025-08-10 13:20:16 +0900

    테스트 3

--- GCP/main.go
+++ GCP/main.go
@@ -12,6 +12,6 @@ import (
 func main() {
 	_ = godotenv.Load()
 	http.Handle("/", http.HandlerFunc(examhandler.ExamHandler))
-	log.Println("서버 시작")
+	log.Println("server start")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
