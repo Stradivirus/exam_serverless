f64571636a49f0b459966dde6701094bcb04bbff
Super User <root@DESKTOP-4G7I13F.localdomain>
2025-08-10 14:08:56 +0900

    테스트 8

--- GCP/main.go
+++ GCP/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/joho/godotenv"
 
 	"example.com/examhandler/examhandler"
 )
+
+func handler(ctx context.Context) (string, error) {
+	// examhandler.ExamHandler를 람다용으로 변환해서 사용
+	return examhandler.ExamHandlerLambda(ctx)
+}
 
 func main() {
 	_ = godotenv.Load()
 	http.Handle("/", http.HandlerFunc(examhandler.ExamHandler))
 	log.Println("server start")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	lambda.Start(handler)
 }
