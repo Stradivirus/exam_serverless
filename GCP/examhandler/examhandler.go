e1ba05725c3470344767f7b198b61b80bc8a13d7
Super User <root@DESKTOP-I98R5CS.localdomain>
2025-08-11 10:31:03 +0900

    test7

--- GCP/examhandler/examhandler.go
+++ GCP/examhandler/examhandler.go
@@ -12,26 +12,35 @@ type LambdaResponse struct {
 }
 
 func ExamHandlerLambda(ctx context.Context) (LambdaResponse, error) {
-    // 실제 비즈니스 로직 처리 (예시: 문제 리스트 반환)
-    result := map[string]interface{}{
-        "result": "ok",
-        // 실제 데이터 추가
+    // DB 연결
+    collection, client, err := getCollection("awssaa") // examType을 파싱해서 전달
+    if err != nil {
+        return LambdaResponse{StatusCode: 500, Body: `{"error":"DB 연결 실패"}`}, nil
+    }
+    defer client.Disconnect(context.Background())
+
+    // 기존 로직 재사용
+    var questions []BaseQuestion
+    cursor, err := collection.Find(context.Background(), bson.M{})
+    if err != nil {
+        return LambdaResponse{StatusCode: 500, Body: `{"error":"문제 조회 실패"}`}, nil
+    }
+    defer cursor.Close(context.Background())
+    if err = cursor.All(context.Background(), &questions); err != nil {
+        return LambdaResponse{StatusCode: 500, Body: `{"error":"문제 파싱 실패"}`}, nil
     }
 
-    bodyBytes, _ := json.Marshal(result)
-
+    bodyBytes, _ := json.Marshal(questions)
     headers := map[string]string{
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type",
         "Content-Type": "application/json",
     }
-
     resp := LambdaResponse{
         StatusCode: 200,
         Headers:    headers,
         Body:       string(bodyBytes),
     }
-
     return resp, nil
 }
